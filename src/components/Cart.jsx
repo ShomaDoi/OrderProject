3a6c740e00c73aa340770313584f1af37a76ac59
sebastian <sebastiangenorth@gmail.com>
2024-09-18 15:13:06 +0200

    moguce povecane i smanjenje broja obroka u Cartu, prikazane cene i kvantitet, Close dugme sada radi ispravno

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,24 +4,24 @@ import { currencyFormmater } from "../utils/format";
 import UserContext from "../store/UserContext";
 import Button from "./UI/Button";
 import Modal from "./UI/Modal";
+import CartItem from "./CartItem";
 export default function Cart() {
   const cartCtx = useContext(CartContext);
   const totalCost = cartCtx.items.reduce(
-    (totalCost, item) => totalCost + item.quantity * item.price
-  , 0);
+    (totalCost, item) => totalCost + item.quantity * item.price,
+    0
+  );
   const userCtx = useContext(UserContext);
   function handleCloseCart() {
     userCtx.hide();
   }
-  console.log(userCtx.progress === 'cart');
+  console.log(cartCtx.items);
   return (
-    <Modal className="cart" open={userCtx.progress === 'cart'}>
+    <Modal className="cart" open={userCtx.progress === "cart"}>
       <h2>Your Cart</h2>
       <ul>
         {cartCtx.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+          <CartItem key={item.id} item={item}></CartItem>
         ))}
       </ul>
       <p className="cart-total">{currencyFormmater.format(totalCost)}</p>
---
+++ src/components/CartItem.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import { currencyFormmater } from "../utils/format";
+import CartContext from "../store/CartContext";
+export default function CartItem({ item }) {
+   const itemCtx = useContext(CartContext);
+  return (
+    <li key={item.id}>
+      <div className="cart-item">
+        <p>{item.name} - {item.quantity} x {currencyFormmater.format(item.price)}</p>
+        <p className="cart-item-actions">
+          <button onClick={()=> itemCtx.removeItem(item.id)}>-</button>
+          <span>{item.quantity}</span>
+          <button  onClick={()=> itemCtx.addItem(item)}>+</button>
+        </p>
+      </div>
+    </li>
+  );
+}
